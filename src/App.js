84f3538dff9923b85a9446cd61b9f3ccc354a670
shrutidhanopiya <75841910+shrutidhanopiya@users.noreply.github.com>
2022-09-14 20:11:11 +0530

    Weather-Info

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import Restro from './component/Restro'
+//import {Layout,Typography,Space} from 'antd'
+//import Navbar from './component/Navbar';
+import './App.css'
+import Temp from './component/weather/temp'
+//  import UseReducer from './component/Hooks/UseReducer'
+// import UseState from './component/Hooks/UseState';
+const App = () => {
+  return (
+  <>
+  {/* <UseState/> */}
+  {/* <Restro/> */}
+  {/* <UseReducer/> */}
+  <Temp/>
+  </>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App"
+import {BrowserRouter as Router} from 'react-router-dom'
+ReactDom.render(
+    <Router>
+         <App/>
+    </Router>
+
+,document.getElementById("root"));
